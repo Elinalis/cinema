495a3f4355b9ed37e423920f58b27e874cd9ce27
Elinalis <elina98@inbox.ru>
2020-09-16 14:28:15 +0300

    Т3-14

---
+++ js/custom.js
@@ -0,0 +1,85 @@
+const initOwl = function() {
+    $(".owl-carousel").owlCarousel({
+        nav:true,
+    });
+}
+
+$(document).ready(initOwl);
+
+const genre_1 = 'фэнтези';
+const genre_2 = 'боевик';
+const genre_3 = 'комедия';
+const genre_4 = 'мультфильм';
+const genre_5 = 'драма';
+const genre_6 = 'фантастика';
+const genre_7 = 'приключения';
+
+
+const films = [
+    {
+        start: '10:00',
+        name: 'Человек-паук',
+        genre: `${genre_6}, ${genre_2}, ${genre_7}`,
+    },
+
+    {
+        start: '12:00',
+        name: 'Собачья жизнь 2',
+        genre: `${genre_1}, ${genre_5}, ${genre_3}`,
+
+    },
+
+    {
+        start: '14:00',
+        name: 'История игрушек 4',
+        genre: `${genre_4}, ${genre_1}, ${genre_3}`,
+    
+    },
+
+    {
+        start: '16:00',
+        name: 'Люди в черном: Интернэшнл',
+        genre: `${genre_6}, ${genre_2}, ${genre_3}`, 
+    },
+
+    
+];
+
+// console.log(films[0]);
+
+const film_start_1 = document.getElementById('film_start_1');
+const film_name_1 = document.getElementById('film_name_1');
+const film_genre_1 = document.getElementById('film_genre_1');
+
+const film_start_2 = document.getElementById('film_start_2');
+const film_name_2 = document.getElementById('film_name_2');
+const film_genre_2 = document.getElementById('film_genre_2');
+
+const film_start_3 = document.getElementById('film_start_3');
+const film_name_3 = document.getElementById('film_name_3');
+const film_genre_3 = document.getElementById('film_genre_3');
+
+const film_start_4 = document.getElementById('film_start_4');
+const film_name_4 = document.getElementById('film_name_4');
+const film_genre_4 = document.getElementById('film_genre_4');
+
+film_start_1.innerHTML = films[0].start;
+film_name_1.innerHTML = films[0].name;
+film_genre_1.innerHTML = films[0].genre;
+
+film_start_2.innerHTML = films[1].start;
+film_name_2.innerHTML = films[1].name;
+film_genre_2.innerHTML = films[1].genre;
+
+film_start_3.innerHTML = films[2].start;
+film_name_3.innerHTML = films[2].name;
+film_genre_3.innerHTML = films[2].genre;
+
+film_start_4.innerHTML = films[3].start;
+film_name_4.innerHTML = films[3].name;
+film_genre_4.innerHTML = films[3].genre;
+
+console.log(film_start_1)
+
+
+
