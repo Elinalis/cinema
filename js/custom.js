cc4a4c9e53b39adc955c1feafd88c8155ebab8c4
Elinalis <elina98@inbox.ru>
2020-10-20 18:41:54 +0300

    Т3-19

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
+const initOwl = function() {
+    $(".owl-carousel").owlCarousel({
+        nav:true,
+    });
+}
+
+
 // const genre_1 = 'фэнтези';
 // const genre_2 = 'боевик';
 // const genre_3 = 'комедия';
@@ -100,6 +107,9 @@
 //     return error;
 // }
 
+
+// Pop-up
+
 const closePopupButton = document.getElementById('popup-close');
 const openPopupButton = document.getElementById('popup-open');
 const sendForm = document.getElementById('submit');
@@ -163,16 +173,235 @@ function checkInput(value) {
 }
 
 
-// const aaa = new Promise ((res, rej) => {
-//     setTimeout(function() {
-//             res(222);
-//     },1000)    
-// }) 
-// aaa.then((two) => {
-//     console.log(two)
-// })
+// Фильмы
 
-// setTimeout(function() {
-//     console.log('111')   
-// },1000)    
+const url = 'https://kinopoiskapiunofficial.tech/api/v2.1/'
+const filmsElement = document.getElementById('posters')
+const tableFilmsElement = document.getElementById("filmsHire")
+const films = [
+    464963,
+    301,
+    535341,
+    2213,
+    1005878,
+    1236063
+];
 
+
+const getFilmById = function (id) {
+    return new Promise (function(resolve, reject){
+        fetch (`${url}/films/${id}`, {
+            headers: {
+                'X-API-KEY': '90a9c8fa-9698-4c1e-9886-2073350b4a64'
+            }
+        }).then(response => response.json()).then(resolve)
+    })
+}
+
+const parseFilm = function (data) {
+    data=data.data;
+    let countries = '';
+    let genres = '';
+    data.countries.forEach(function(item){
+        countries += `${item.country}`
+    });
+    data.genres.forEach(function(item){
+        genres += `${item.genre} `
+    });
+
+    return {
+        name: data.nameRu,
+        country: countries,
+        genre: genres,
+        year: data.year,
+        description: data.description,
+        img: data.posterUrl,
+        link: data.webUrl
+    }
+}
+
+const film = 
+{
+    getName: function() {
+        return this.nameRu;
+    },
+
+    getCountry: function() {
+        let countries = '';
+        this.countries.forEach(function(item){
+            countries += `${item.country} `
+        });
+        return countries
+    },
+
+    getYear: function() {
+        return this.year;
+    },
+
+    getDescription: function() {
+        return this.description;
+    },
+
+    getImg: function() {
+        return this.posterUrl;
+    },
+
+    getLink: function() {
+        return this.webUrl;
+    },
+
+    getStart: function() {
+        function getRandomNumber(min, max){
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random()*(max-min+1)) + min;
+        }
+    
+        let time1, time2, time3, time4;
+    
+        time1 = getRandomNumber(0, 2);
+        time3 = getRandomNumber(0, 5);
+        time4 = getRandomNumber(0, 9)
+    
+            switch(time1){
+            case 0:
+            case 1:
+                time2 = getRandomNumber(0, 9)
+            default:
+                time2 = getRandomNumber(0, 3)
+        }
+    
+        return `${time1}${time2} : ${time3}${time4}`
+    },
+
+    getGenre: function() {
+        let genres = '';
+        this.genres.forEach(function(item){
+            genres += `${item.genre}`
+        });
+        return genres;
+
+    },
+
+    // renderFilmRow: function() {
+    //    return `<tr class="table-body__row">
+    //    <td>
+    //      <input type="checkbox" class= "block-table__checkbox">
+    //      <label for="block-table__checkbox"><i class= "icon icon-check" title="check"></i></label>
+    //    </td>
+    //    <td id="film_start_1" class="table__row-time">${film.getStart.call(this)}</td>
+    //    <td id="film_name_1">${this.nameRu}</td>
+    //    <td id="film_genre_1">${film.getGenre.call(this)}</td>
+    //     </tr>`
+    // },
+
+    // renderFilmBlock: function() {
+    //     return `<div class="block-films__movie">
+    //     <div class="block-films__relative">
+    //         <div class="block-films__table__bg"><img class="link-img" src="${this.getImg}"></div>
+    //             <div class="block-films__table__description">
+    //             <div class="block-films__table__header">${this.name}</div>
+    //             <div class="block-films__table__sep"></div>
+    //             <div class="block-films__table__text">${this.description}</div>
+    //             <div class="block-films__table__links">
+    //             <a href='https://www.facebook.com/' target="_blank"><i class="icon" title="facebook">
+    //                 <img src="icons/facebook.png">
+    //                 </i></a>
+    //             <a href='https://twitter.com/' target="_blank"><i class="icon" title="twitter">
+    //                 <img src="icons/twitter.png">
+    //                 </i></a>
+    //             <a href='https://www.behance.net/' target="_blank"><i class="icon" title="behance">
+    //                 <img src="icons/behance.png">
+    //                 </i></a>
+    //             <a href='https://dribbble.com/' target="_blank"><i class="icon" title="dribbble">
+    //                 <img src="icons/dribbble.png">
+    //                 </i></a>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>`;     
+    // }
+
+}
+
+
+
+const generateFilmItem = function ({name, country, genre, year, description, img, link}) {
+    return `<div class="block-films__movie">
+                <div class="block-films__relative">
+                    <div class="block-films__table__bg"><img class="link-img" src="${img}"></div>
+                        <div class="block-films__table__description">
+                        <div class="block-films__table__header">${name}</div>
+                        <div class="block-films__table__sep"></div>
+                        <div class="block-films__table__text">${description}</div>
+                        <div class="block-films__table__links">
+                        <a href='https://www.facebook.com/' target="_blank"><i class="icon" title="facebook">
+                            <img src="icons/facebook.png">
+                            </i></a>
+                        <a href='https://twitter.com/' target="_blank"><i class="icon" title="twitter">
+                            <img src="icons/twitter.png">
+                            </i></a>
+                        <a href='https://www.behance.net/' target="_blank"><i class="icon" title="behance">
+                            <img src="icons/behance.png">
+                            </i></a>
+                        <a href='https://dribbble.com/' target="_blank"><i class="icon" title="dribbble">
+                            <img src="icons/dribbble.png">
+                            </i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>`;     
+}
+
+const generateTableItem = function ({name, genre}){
+    function getRandomNumber(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random()*(max-min+1)) + min;
+    }
+
+    let time1, time2, time3, time4;
+
+    time1 = getRandomNumber(0, 2);
+    time3 = getRandomNumber(0, 5);
+    time4 = getRandomNumber(0, 9)
+
+        switch(time1){
+        case 0:
+        case 1:
+            time2 = getRandomNumber(0, 9)
+        default:
+            time2 = getRandomNumber(0, 3)
+    }
+
+    const time = `${time1}${time2} : ${time3}${time4}`
+
+        return `<tr class="table-body__row">
+            <td>
+            <input type="checkbox" class="block-table__checkbox" id="tick" value="yes">
+            <label for="tick"><i class= "icon icon-check" title="check"></i></label>
+            </td>
+            <td id="film_start_1" class="table__row-time">${time}</td>
+            <td id="film_name_1">${name}</td>
+            <td id="film_genre_1">${genre}</td>
+          </tr>`       
+};
+
+
+let element, prepareFilm;
+const requests = [];
+films.forEach(function(item, i, arr){
+    let film = getFilmById(item)
+        film.then(result => {
+            // let name = film.renderFilmRow.call(result.data)
+            // console.log(name)
+            prepareFilm = parseFilm(result);
+            element = generateFilmItem({...prepareFilm});
+            tableElement = generateTableItem({...prepareFilm})
+            filmsElement.insertAdjacentHTML('beforeEnd', element)
+            tableFilmsElement.insertAdjacentHTML('beforeEnd', tableElement)
+        })
+    requests.push (film)
+})
+console.log(requests)
+Promise.all(requests).then(()=>{initOwl()})
